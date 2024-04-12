372fe27b056588e81e5c0c5e3865ced9979e51f9
AlinaDragu <alina_elena14@yahoo.com>
2024-04-12 12:35:12 +0300

    score

---
+++ score/score.py
@@ -0,0 +1,8 @@
+from cs50 import get_int # type: ignore
+scores = []
+for i in range(3):
+    score = get_int("Score: ")
+    scores = scores + [score]
+    
+average = sum(scores) / len(scores)
+print(f"Average: {average}")
